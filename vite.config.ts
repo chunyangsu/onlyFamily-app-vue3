dfbe5b18bde2c58b2f3b94eae2015032321ede2a
scy <13057160969@163.com>
2026-06-21 18:19:30 +0800

    feat: 配置按需自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+// 引入自动导入插件
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
@@ -7,9 +9,19 @@ export default defineConfig(async () => {
 
   return {
     plugins: [
+      // uni-app 插件
       uni(),
       // 配置 UnoCSS
       UnoCss(),
+      // 配置自动导入插件
+      AutoImport({
+        imports: ['vue', 'uni-app'], // 自动导入 Vue 和 UniApp 的 API
+        dts: 'src/types/auto-imports.d.ts', // 自动生成类型声明文件
+        eslintrc: {
+          enabled: true, // 生成 ESLint 配置文件
+          filepath: './.eslintrc-auto-import.json', // ESLint 配置文件路径
+        },
+      }),
     ],
   }
 })
