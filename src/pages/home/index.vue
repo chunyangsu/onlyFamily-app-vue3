11cb8bfd8a57c348ef8b62d7673e4747b70d305e
scy <13057160969@163.com>
2026-06-21 16:07:51 +0800

    feat: 安装 wot ui 组件库

---
+++ src/pages/home/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <view class="content">
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+    <wd-button type="success">成功按钮22</wd-button>
+    <wd-icon name="add-circle" />
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Hello scy')
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
